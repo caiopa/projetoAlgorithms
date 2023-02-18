e6c3b4cb128230de58867d1dedc26aa409815924
Caio Pereira Antunes <caioefg@gmail.com>
2023-02-18 12:41:57 -0300

    test: criação do test para funçao encrypt req2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    message = 'caiopereira'
+    invalid_key = "1"
+    invalid_message = 1
+
+    assert encrypt_message(message, 4) == 'arierep_oiac'
+    assert encrypt_message(message, 11) == 'arierepoiac'
+
+    with pytest.raises(TypeError):
+        encrypt_message(message, invalid_key)
+    with pytest.raises(TypeError):
+        encrypt_message(invalid_message, 4)
